498a3c0b72febb3a946814166bb016d8a66d95fc
Yuri Costa <yuri@sparkroad.com>
2015-08-09 00:21:09 -0700

    added more operators

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -28,12 +28,75 @@ class ViewController: UIViewController {
             resultsLabel.text = digit
             userIsInTheMiddleOfTypingANumber = true
         }
+        sender.frame = CGRectMake(10, 10, 10, 10)
     }
     
-    //@IBAction func appendDigitWithEvent(sender: UIButton, forEvent event: UIEvent) {
-    //}
+    var operandStack = Array<Double>()
     
+    @IBAction func Enter()
+    {
+        userIsInTheMiddleOfTypingANumber = false
+        operandStack.append(displayValue)
+        println("OperandStack = \(operandStack)")
+    }
+
+    //setting up display value as numbers and getting it to flow back to the calculator's display
+    var displayValue: Double
+    {
+        get
+        {
+            return NSNumberFormatter().numberFromString(resultsLabel.text!)!.doubleValue
+        }
+        set
+        {
+            resultsLabel.text = "\(newValue)"
+            userIsInTheMiddleOfTypingANumber = false
+        }
+    }
+    //Time to crunch the stack
+    @IBAction func operate(sender: UIButton)
+    {
+        var operation = sender.currentTitle!
+        if userIsInTheMiddleOfTypingANumber
+        {
+            Enter()
+        }
+        switch operation
+        {
+        case "+": performOperation { $0 + $1}
+        case "-": performOperation { $0 - $1}
+        case "×": performOperation { $1 * $0}
+        case "÷": performOperation { $1 / $0}
+        case "√": performOperation { sqrt($0)}
+            
+        default: break
+        }
+        
+        
+    }
+    // generic calculator function
+    //TODO: Right now if there are less than 2 items on the stack it just copies the last one and it will now have 2
+    private func performOperation (operation: (Double, Double) -> Double)
+    {
+        if operandStack.count >= 2
+        {
+            displayValue = operation(operandStack.removeLast(), operandStack.removeLast())
+        }
+        Enter()
+    }
     
+    // generic calculator function for one argument calls
+    private func performOperation(operation: Double -> Double)
+    {
+        if operandStack.count >= 1
+        {
+            displayValue = operation(operandStack.removeLast())
+        }
+        Enter()
+    }
+    
+
+
     
     
     
